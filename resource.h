d2e7db1485774cecf556cb302e4f9bf55278d684
Diomidis Spinellis <dds@aueb.gr>
2021-03-12 23:34:11 +0200

    Initiali revision
    Copy-pasted from
    https://www.dreamincode.net/forums/topic/261009-bitmap-printing-tutorial-in-c-win32/

---
+++ resource.h
@@ -0,0 +1,13 @@
+#define WINVER 0x0500
+#define _WIN32_WINNT 0x0500
+
+#include <windows.h>
+
+#define ICO1           100
+
+#define hMenu          1000
+#define IDM_OPEN_BM    1001
+#define IDM_PRINT_BM   1002
+#define IDM_SAVE_BM    1003
+#define IDM_EXIT       1004
+
